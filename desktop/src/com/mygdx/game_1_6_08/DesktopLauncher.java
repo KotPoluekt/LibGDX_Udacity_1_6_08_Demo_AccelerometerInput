8b236e8c06586ed5ba694114bbc2c258b7c8955f
Alex Biryukov <birukov.alex@gmail.com>
2022-08-19 11:49:52 +0500

    Initial commit

---
+++ desktop/src/com/mygdx/game_1_6_08/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game_1_6_08;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.game_1_6_08.MyGdxGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("1_6_08_Demo_AccelerometrInput");
+		new Lwjgl3Application(new MyGdxGame(), config);
+	}
+}
